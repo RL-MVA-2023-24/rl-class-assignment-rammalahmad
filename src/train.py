da2d6d1cdb07b3c397504e5ef48778171976cf37
rammalahmad <rammal.ahmad@outlook.fr>
2024-03-01 00:17:02 +0100

    first DQN

--- src/train.py
+++ src/train.py
@@ -1,6 +1,12 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
+import numpy as np
+import random
+from collections import deque
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
@@ -11,11 +17,51 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
-    def act(self, observation, use_random=False):
-        return 0
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        """Neural Net for Deep-Q learning Model."""
+        model = models.Sequential()
+        model.add(layers.Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(layers.Dense(24, activation='relu'))
+        model.add(layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=tf.optimizers.Adam(learning_rate=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state, use_random=False):
+        if np.random.rand() <= self.epsilon or use_random:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns the best action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
     def save(self, path):
-        pass
+        self.model.save(path)
 
-    def load(self):
-        pass
+    def load(self, path):
+        self.model = models.load_model(path)
